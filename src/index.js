515a728059dfecb41d566eacf5bf84dabf78d3f6
Adrian <forever0754@gmail.com>
2023-11-06 14:01:49 +0100

    aws https configured

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const app = require('./app')
 const port = process.env.PORT || 5000;
 
+const certificate = fs.readFileSync('../certificate.crt');
+const privateKey = fs.readFileSync('../private.key');
+
 try {
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
+} catch (err) {
+  console.log(err)
+}
+
+try {
+  https.createServer({
+    key: privateKey,
+    cert: certificate,
+  }, app).listen(8443);
 } catch (err) {
   console.log(err)
 }
